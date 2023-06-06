64f46e6ce3c0ba1890b797a49c5b1105e95cf93f
Zanetti Jordan <jzanetti1@outlook.fr>
2023-06-06 09:30:17 +0200

    Add fonts & colors variables

---
+++ src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1>Moohvies</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
