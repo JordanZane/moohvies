3036aa33de15d8b996743861a5f325e85d4ac308
Zanetti Jordan <jzanetti1@outlook.fr>
2023-06-06 09:56:00 +0200

    Add react  bootstrap downdown menu

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { Dropdown } from 'react-bootstrap';
+import logoImg from '../assets/images/cow.svg';
 
 const Header = () => {
   return (
@@ -6,7 +9,39 @@ const Header = () => {
       <div className="container">
         <div className="row">
           <div className="col-md-12">
-            <h1>Moohvies</h1>
+            <h1>
+              <Link to="/">
+                Moohvies
+                <img src={logoImg} alt="Moohvies" />
+              </Link>
+            </h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <nav className="menu">
+              <ul>
+                <li>
+                  <NavLink to="/">Home</NavLink>
+                </li>
+                <Dropdown>
+                  <Dropdown.Toggle variant="secondary" id="dropdownMenuButton">
+                    Genres
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item as={NavLink} to="/genres/action">
+                      Action
+                    </Dropdown.Item>
+                    <Dropdown.Item as={NavLink} to="/genres/scifi">
+                      Sci-Fi
+                    </Dropdown.Item>
+                    <Dropdown.Item as={NavLink} to="/genres/comedy">
+                      Comedy
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </ul>
+            </nav>
           </div>
         </div>
       </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../Components/Header';
 
 const Home = () => {
   return (
-    <div>
+    <>
+      <Header />
       <h1>Home</h1>
-    </div>
+    </>
   );
 };
 
