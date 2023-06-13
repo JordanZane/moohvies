3fcaea7f148e4661f82f797d44e38c30e5c93ffc
Zanetti Jordan <jzanetti1@outlook.fr>
2023-06-13 10:37:31 +0200

    Add styles SM menu

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -18,7 +18,7 @@ const Navigation = () => {
         <div className="row">
           <div className="col-md-12">
             <nav className="menu">
-              <ul>
+              <ul className="dk-menu">
                 <li>
                   <NavLink to="/">Home</NavLink>
                 </li>
@@ -78,63 +78,70 @@ const Navigation = () => {
                 </li>
               </ul>
               <ul className="sm-menu">
-                <li>
-                  <NavLink to="/">Home</NavLink>
-                </li>
-                <li>
-                  <Dropdown>
-                    <Dropdown.Toggle id="dropdownMenuButton">
-                      Genres
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                      <Dropdown.Item as={NavLink} to="/genres/Action">
-                        Action
-                      </Dropdown.Item>
-                      <Dropdown.Item as={NavLink} to="/genres/Sci-fi">
-                        Sci-Fi
-                      </Dropdown.Item>
-                      <Dropdown.Item as={NavLink} to="/genres/Comedy">
-                        Comedy
-                      </Dropdown.Item>
-                      <Dropdown.Item as={NavLink} to="/genres/Drama">
-                        Drama
-                      </Dropdown.Item>
-                      <Dropdown.Item as={NavLink} to="/genres/Romance">
-                        Romance
-                      </Dropdown.Item>
-                      <Dropdown.Item as={NavLink} to="/genres/Horror">
-                        Horror
-                      </Dropdown.Item>
-                      <Dropdown.Item as={NavLink} to="/genres/Thriller">
-                        Thriller
-                      </Dropdown.Item>
-                      <Dropdown.Item as={NavLink} to="/genres/Crime">
-                        Crime
-                      </Dropdown.Item>
-                      <Dropdown.Item as={NavLink} to="/genres/Mystery">
-                        Mystery
-                      </Dropdown.Item>
-                      <Dropdown.Item as={NavLink} to="/genres/Familly">
-                        Familly
-                      </Dropdown.Item>
-                      <Dropdown.Item as={NavLink} to="/genres/Documentary">
-                        Documentary
-                      </Dropdown.Item>
-                      <Dropdown.Item as={NavLink} to="/genres/Western">
-                        Western
-                      </Dropdown.Item>
-                      <Dropdown.Item as={NavLink} to="/genres/Animation">
-                        Animation
-                      </Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-                </li>
-                <li>
-                  <NavLink to="/most-popular">Most popular</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/top-rated">Top rated</NavLink>
-                </li>
+                <Dropdown>
+                  <Dropdown.Toggle id="dropdownMenuBurger">
+                    <i class="fa-solid fa-bars"></i>
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <li>
+                      <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                      <Dropdown>
+                        <Dropdown.Toggle id="dropdownMenuButton">
+                          Genres
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                          <Dropdown.Item as={NavLink} to="/genres/Action">
+                            Action
+                          </Dropdown.Item>
+                          <Dropdown.Item as={NavLink} to="/genres/Sci-fi">
+                            Sci-Fi
+                          </Dropdown.Item>
+                          <Dropdown.Item as={NavLink} to="/genres/Comedy">
+                            Comedy
+                          </Dropdown.Item>
+                          <Dropdown.Item as={NavLink} to="/genres/Drama">
+                            Drama
+                          </Dropdown.Item>
+                          <Dropdown.Item as={NavLink} to="/genres/Romance">
+                            Romance
+                          </Dropdown.Item>
+                          <Dropdown.Item as={NavLink} to="/genres/Horror">
+                            Horror
+                          </Dropdown.Item>
+                          <Dropdown.Item as={NavLink} to="/genres/Thriller">
+                            Thriller
+                          </Dropdown.Item>
+                          <Dropdown.Item as={NavLink} to="/genres/Crime">
+                            Crime
+                          </Dropdown.Item>
+                          <Dropdown.Item as={NavLink} to="/genres/Mystery">
+                            Mystery
+                          </Dropdown.Item>
+                          <Dropdown.Item as={NavLink} to="/genres/Familly">
+                            Familly
+                          </Dropdown.Item>
+                          <Dropdown.Item as={NavLink} to="/genres/Documentary">
+                            Documentary
+                          </Dropdown.Item>
+                          <Dropdown.Item as={NavLink} to="/genres/Western">
+                            Western
+                          </Dropdown.Item>
+                          <Dropdown.Item as={NavLink} to="/genres/Animation">
+                            Animation
+                          </Dropdown.Item>
+                        </Dropdown.Menu>
+                      </Dropdown>
+                    </li>
+                    <li>
+                      <NavLink to="/most-popular">Most popular</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/top-rated">Top rated</NavLink>
+                    </li>
+                  </Dropdown.Menu>
+                </Dropdown>
               </ul>
               <form onSubmit={handleSearch}>
                 <button type="submit">
